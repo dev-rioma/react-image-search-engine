df5002ec86f7621f8193a4d503f82c9949baa7b3
dev-rioma <dev.rioma@gmail.com>
2023-01-13 11:21:57 -0500

    create app search engine images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({ photos })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: ''}}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, { 
+              headers: {
+                'Authorization': 'Client-ID iI83adI76mBrt7ljkjsL3aajw7KQbEO9riSiBzU5Jxs'
+              }
+            })
+            const data = await response.json()
+            // llamar a la API
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+
+        </Formik>
       </header>
+      <div className='container'>
+        <div className='center'>
+          {photos.map(photo => 
+            <article 
+              key={photo.id} 
+              onClick={() => 
+                open(photo.links.html)}
+            >
+              <img src={photo.urls.regular} />
+              <p>
+                {[photo.description, photo.alt_description].join(' - ')}
+              </p>
+            </article>)}
+        </div>
+      </div>
     </div>
   );
 }
